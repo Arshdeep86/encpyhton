aa0ca264c14a0e57b0bdcc8d5886a4f7be41ca74
Arshdeep kaur <arshdeep86>
2020-01-19 21:30:28 +0530

    session4c

---
+++ session4c.py
@@ -0,0 +1,19 @@
+cabPrice = 100
+wallet = 500
+print("Can i book cab: ", (cabPrice <= wallet))
+
+
+usrname = input("enter username: ")
+password = input("enter password")
+if usrname == "arsh" and password == "123":
+    print("Login successfully")
+else:
+    print("authentication failed")
+
+
+
+otp = int(input(print("enter OTP:")))
+print("is otp 123 :", (otp == 123))
+print("your otp is not 123 :", (otp !=123))
+
+
