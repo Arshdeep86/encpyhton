9054fb9843150006c07af3aa792e110d7b4c6e6c
Arshdeep kaur <arshdeep86>
2020-01-19 20:44:51 +0530

    comparison operators

---
+++ CompOperators.py
@@ -0,0 +1,16 @@
+# Comparison operators in pyhton
+num1 = 100
+num2 = 200
+
+res = num1 == num2
+print(" num1 == num2 :", res)
+res = num1 !=num2
+print("num1 != num2: ", res)
+res = num1 < num2
+print("num1 < num2: ", res)
+res = num1 > num2
+print("num1 > num2: ", res)
+res = num1 <= num2
+print("num1 <= num2: ", res)
+res = num1 >= num2
+print("num1 >= num2: ", res)
