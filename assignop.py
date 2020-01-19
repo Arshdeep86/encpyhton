f09c0a4230bc647057c8341e942cf947c9a421cd
Arshdeep kaur <arshdeep86>
2020-01-19 20:32:56 +0530

    assignment oprators

---
+++ assignop.py
@@ -0,0 +1,21 @@
+# Assignmnt operators in pyhton
+num1 = 4       # equal assignment operator
+num2 = 8
+res = 0
+res += num1    #res = res+num1
+print("after assignment addition result is: ", res)
+
+res -= num1
+print("after assignment subtraction result is: ", res)
+
+res /= num1   # res = 0/4
+print("after division assignment result is: ", res)
+
+res //= num1
+print("after floor division assignment result is:", res )
+
+num2 %= num1    # num2 = 8/4 remainder 0
+print("after remainder assignment is: ", res)
+
+
+
