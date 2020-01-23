e065d49f3c0af1466b36f387090a6ed0c5787853
Arshdeep kaur <arshdeep86>
2020-01-23 13:11:12 +0530

    basic set operation

---
+++ session8G.py
@@ -0,0 +1,20 @@
+s1 = {'a','b',1,2}
+print(s1)
+s1.add('x')
+print(s1)
+s1.remove(1)
+print(s1)
+s1.pop()   #pops first element of set
+print(s1)
+s1.clear()
+print(s1)
+
+data = [1,2,3]
+s = set(data)    # convert list into set
+print(s)
+list1 = [2,3,5,]
+list1.clear()
+print(list1)
+set1 = {1,2,3,4,'a',1,4}
+list2 = list(set1)
+print(list2)
