c51447ca82e31b415a40aee0c9382d690ce9a894
Arshdeep kaur <arshdeep86>
2020-01-23 13:03:09 +0530

    set function

---
+++ sets.py
@@ -0,0 +1,10 @@
+s1 = {1,2,3,4,5,2,}
+s2 = {3, 4, 5,6 }
+s3 = s1| s2  # union
+print(s3)
+s4 = s1 & s2  #intersection
+print(s4)
+s5 = s1 - s2   # Difference
+print(s5)
+print("******************************************************")
+
