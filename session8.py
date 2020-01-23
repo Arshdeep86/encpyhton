807293ee644e702535e913300bdc733b568078e2
Arshdeep kaur <arshdeep86>
2020-01-23 20:02:57 +0530

    strings in python with escape sequence

---
+++ session8.py
@@ -0,0 +1,35 @@
+johnsShop ='Johns Cofee Shop'
+# print(johnsShop, type(johnsShop))
+#  RAW string { which will ignore escape sequences in string}
+johnsShop = r'John\'s Cofee shop'
+print(johnsShop, type(johnsShop))
+
+
+message = "this is nice day" \
+          "we will code today" \
+          "thank you:)"
+print(message)
+name = "john watson"
+print(name, type(name), hex9(id(name)))
+#name is a  reference variable which wil hold hashcode of  john watson
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
