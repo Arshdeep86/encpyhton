1acdf871d994819ebefce7721068584cd5020c9c
Arshdeep kaur <arshdeep86>
2020-01-28 17:32:23 +0530

    set operations

---
+++ session7c.py
@@ -0,0 +1,23 @@
+
+# set operations
+students = {"john", "jennie", "jim", "john", "jack", "joe"}
+print(students)
+print(type(students))
+
+#print(students + {"arsh", "neha"}
+#newstudents = students + {"arsh", "neha"}
+
+#print(students[0])
+#print(students[len(students)-1])
+
+#for i in range(0, len(students)):
+#print(students[i])
+
+for student in students:
+    print(student)
+
+
+
+
+
+
