4eda6b9e285960d0a883d94cf9f73118ad4d6548
Arshdeep kaur <arshdeep86>
2020-02-06 20:20:03 +0530

    dart language heloo print program

---
+++ practice1.py
@@ -0,0 +1,12 @@
+
+DartProgram = """
+main(){
+ print("W3Adda - Hello World!");
+}
+"""
+print(DartProgram)
+file1 = open("/Users/Arshdeep kaur/Desktop/myapp.dart","w")
+file1.write(DartProgram)
+file1.close()
+print("Program created")
+5e
